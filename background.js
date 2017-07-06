e42254272d26a07ba39a6fd32f142fdaf12c1b79
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2017-07-06 21:09:36 +0300

    First commit

---
+++ background.js
@@ -0,0 +1,95 @@
+//Set Google Analytics tracking
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-74097617-16']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+var menuItem = {
+    "id": "Serpstat2",
+    "title": "Serpstat проверка на дума",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.remove('Serpstat2', function() {
+    chrome.contextMenus.create(menuItem);
+});
+
+function fixedEncodeURI (str) {
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "Serpstat2" && clickData.selectionText){
+        var serpstatUrl = "https://serpstat.com/keywords/index/?search_type=keyword&query=" + fixedEncodeURI(clickData.selectionText) +"&se=g_bg";
+        var createData = {
+            "url": serpstatUrl,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth,
+            "height": screen.availHeight/1.5
+        };
+        chrome.windows.create(createData, function(){});
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var headings = document.getElementById('headings');
+    var robots = document.getElementById('robots');
+    var home = document.getElementById('home');
+    var images = document.getElementById('images');
+    var prev_next = document.getElementById('prev-next');
+    var hreflang = document.getElementById('hreflang');
+    var all_html = document.getElementById('all-html');
+
+    headings.addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code: 'var a,b,c,d,e,f;f=new Array("pink","orange","yellow","aquamarine","lightskyblue","plum");for(a=1;a<=6;a++){b=document.getElementsByTagName("h"+a);for(c=0;c<b.length;c++){d=b[c];e=d.style;e.backgroundColor=f[a-1];e.border="1px solid black";e.color="black";d.innerHTML="H"+a+" "+d.innerHTML;}}'
+        });
+    });
+
+    robots.addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code: 'location.href="http://"+location.host+"/robots.txt"'
+        });
+        document.getElementById("refresh").innerHTML = "Обратно";
+    });
+
+    refresh.addEventListener('click', function () {
+        document.getElementById("refresh").innerHTML = "Рефреш";
+        chrome.tabs.executeScript({
+            code: 'window.location.href = location.origin'
+        });
+    });
+
+    images.addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code: 'var imgs = document.getElementsByTagName("img"); var iAlt = ""; var iTitle = ""; for (var i=0; i<imgs.length; i++) { imgs[i].setAttribute("width", imgs[i].width); imgs[i].setAttribute("height", imgs[i].height); imgs[i].removeAttribute("src"); imgs[i].removeAttribute("srcset"); imgs[i].removeAttribute("className"); if (imgs[i].getAttribute("alt") != null) iAlt = imgs[i].alt; else iAlt = ""; if (iAlt == "") iAlt = "empty"; if (imgs[i].getAttribute("title") != null) iTitle = imgs[i].title; else iTitle = ""; if (iTitle == "") iTitle = "empty"; imgs[i].alt = "Alt: "+iAlt+" Title: "+iTitle; }'
+        });
+    });
+
+    prev_next.addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code: 'var url = document.location; var links = document.getElementsByTagName("link"); var fprev = "none"; var fnext = "none"; for (var i = 0, l; l = links[i]; i++) { if (l.getAttribute("rel") == "prev") { fprev = l.getAttribute("href"); } if (l.getAttribute("rel") == "next") { fnext = l.getAttribute("href"); } } alert("prev: "+fprev+"\\r\\nnext: "+fnext); '
+        });
+    });
+
+    hreflang.addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code:'var links=document.getElementsByTagName("link"); var href="none"; var hreflang="none"; var s=""; for(var i=0;i<links.length;i++){ if(links[i].getAttribute("rel")=="alternate"){href=links[i].getAttribute("href"); hreflang=links[i].getAttribute("hreflang"); if(href!=null&&hreflang!=null) { s = s + "href: "+href+"\\r\\n"+"hreflang: "+hreflang+"\\r\\n-------------------------------------\\r\\n"}}} if(s=="") {s="Няма намерен hreflang"} alert(s);'
+        });
+    });
+
+    all_html.addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code: 'function makeHttpObject(){return new XMLHttpRequest()}var html;var request=makeHttpObject();request.open(\"GET\",location.href,true);request.send(null);request.onreadystatechange=function(){if(request.readyState==4)html=request.responseText};setTimeout(function(){var body_section=html.substring(html.search(\/<body\/i));var current_domain=location.origin;var domains=body_section.match(\/(href=\"https?:\\\/\\\/[^\\s]+)\/gi);var external_domains_count=0;var external_domains=[];for(var i=0,len=domains.length;i<len;i++){domains[i]=domains[i].replace(\"href=\\\"\",\"\");domains[i]=domains[i].substring(0,domains[i].search(\"\\\"\"));domains[i]=domains[i].substring(0,domains[i].indexOf(\"\/\",12));if(!domains[i].startsWith(current_domain)&&domains[i].length>12){external_domains.push(domains[i])}}var external_domains=external_domains.filter(function(elem,index,self){return index==self.indexOf(elem)});var result_external_domains=\"\";for(var i=0,len=external_domains.length;i<len;i++){if(i<5){result_external_domains+=external_domains[i]+\"\\n\\r\"}external_domains_count++}alert(\"\u041E\u0442\u043A\u0440\u0438\u0442\u0438 \u0441\u0430 \u043B\u0438\u043D\u043A\u043E\u0432\u0435 \u043A\u044A\u043C: \"+external_domains_count+\" \u0432\u044A\u043D\u0448\u043D\u0438 \u0434\u043E\u043C\u0435\u0439\u043D\u0430 \u0438\u043B\u0438 \u0433\u0440\u0435\u0448\u043D\u0438 \u0432\u044A\u0442\u0440\u0435\u0448\u043D\u0438 \u0441\u0442\u0440\u0430\u043D\u0438\u0446\u0438! \u0427\u0430\u0441\u0442 \u043E\u0442 \u0442\u044F\u0445:\\n\\r\"+result_external_domains+\"\\n\\r\u041D\u0435\u043E\u0431\u0445\u043E\u0434\u0438\u043C \u0435 \u0430\u043D\u0430\u043B\u0438\u0437, \u0437\u0430 \u0434\u0430 \u0441\u0435 \u0443\u0441\u0442\u0430\u043D\u043E\u0432\u0438 \u043D\u0443\u0436\u0434\u0430\u0442\u0430 \u043E\u0442 \u0438\u043D\u0442\u0435\u0440\u0432\u0435\u043D\u0446\u0438\u044F.\")},2000);'
+        });
+    });
+
+});
---
+++ popup.js
@@ -0,0 +1,124 @@
+var tab_title = '';
+
+function display_canonical(results) {
+    canonical = results;
+    document.querySelector("#meta-title").innerHTML = "<p class='info'><span class='info-heading'>Мета Заглавие: </span>" + tab_title + "</p><p class='info'><span class='info-heading'>Каноничен: </span>" + canonical + "</p>";
+}
+
+function display_h1(results) {
+    h1 = results;
+    document.querySelector("#h1").innerHTML = "<p class='info'><span class='info-heading'>H1 заглавие: </span>" + h1 + "</p>";
+}
+
+function display_netpeak_cloud(results) {
+    netpeak_cloud = results;
+    document.querySelector("#netpeak-cloud").innerHTML = "<p class='info'><span class='info-heading'>ID: </span>" + netpeak_cloud + "</p>";
+}
+
+function http_headers(results) {
+    http_headers = results;
+    table ="<table class='headers-table'>";
+    for (var i = 0, len = http_headers[0].length; i < len; i++){
+        table +="<tr><td>"+http_headers[0][i]+"</td></tr>";
+    }
+    table +="</table>";
+    document.querySelector("#headers-panel").innerHTML = "<p class='info'><span class='info-heading'></span>" + table + "</p>";
+}
+
+function robots_links(results) {
+    var link = results;
+    //document.querySelector("#robots").href=link+"/robots.txt";
+    //document.querySelector("#robots").setAttribute('target', '_blank')
+}
+
+function display_index_noindex(results) {
+    index_noindex = results;
+    var index_noindex_result = document.querySelector("#index-noindex");
+    index_noindex_result.innerHTML = "<p class='info'><span class='info-heading'>Мета index/noindex: </span>" + index_noindex + "</p>";
+    if (index_noindex[0].indexOf("noindex") !== -1) {
+        index_noindex_result.className += "noindex";
+    }
+}
+
+function display_long_meta_error(results) {
+    meta_title_long = results;
+    if(meta_title_long.length>10){
+        document.querySelector("#recommendations-panel-2").innerHTML += "<p class='warning'>"+"Внимание! Прекалено дълъг Meta Title. Текуща дължина: " + meta_title_long.length +" символа.</p>";
+    }
+}
+
+function display_h1_error(results) {
+    h1_error = results;
+    document.querySelector("#recommendations-panel").innerHTML = "<p class='warning'>" + h1_error + "</p>";
+}
+
+function display_meta_description(results) {
+    meta_description = results;
+    document.querySelector("#meta-description").innerHTML = "<p class='info'><span class='info-heading'>Мета описание: </span>" + meta_description + "</p>";
+}
+
+function current_url(results) {
+    current_url = results;
+    current_url_encoded=encodeURIComponent(current_url);
+    document.querySelector("#structured-data").href = "https://developers.google.com/structured-data/testing-tool/?url="+current_url_encoded;
+    document.querySelector("#structured-data").setAttribute('target', '_blank');
+
+    document.querySelector("#page-speed").href = "https://developers.google.com/speed/pagespeed/insights/?hl=bg&url="+current_url_encoded;
+    document.querySelector("#page-speed").setAttribute('target', '_blank');
+
+    document.querySelector("#serpstat").href = "https://serpstat.com/urls/index?query="+current_url_encoded+"&ff=1&search_type=subdomains&se=g_bg";
+    document.querySelector("#serpstat").setAttribute('target', '_blank');
+
+    document.querySelector("#mobile-friendly").href = "https://search.google.com/test/mobile-friendly?url="+current_url_encoded;
+    document.querySelector("#mobile-friendly").setAttribute('target', '_blank');
+
+    document.querySelector("#w3-validator").href = "https://validator.w3.org/nu/?doc="+current_url_encoded;
+    document.querySelector("#w3-validator").setAttribute('target', '_blank');
+
+}
+
+
+chrome.tabs.query({active: true}, function (tabs) {
+    var tab = tabs[0];
+    tab_title = tab.title;
+
+    chrome.tabs.executeScript(tab.id, {
+        code: 'location.href'
+    }, current_url);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: 'var url = document.location; var links = document.getElementsByTagName("link"); var found = "none"; for (var i = 0, l; l = links[i]; i++) { if (l.getAttribute("rel") == "canonical") { found = l.getAttribute("href"); break; } } decodeURIComponent(found);'
+    }, display_canonical);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: 'document.querySelector("h1").textContent'
+    }, display_h1);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: 'var metas = document.getElementsByTagName("meta"); var meta_description=""; for (var i=0; i<metas.length; i++) { if (metas[i].getAttribute("name") == "description") { meta_description = metas[i].getAttribute("content"); } } meta_description;'
+    }, display_meta_description);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: 'var url = document.location; var links = document.getElementsByTagName("meta"); var found = "none"; for (var i = 0, l; l = links[i]; i++) { if (l.getAttribute("name") == "robots") { found = l.getAttribute("content"); break; } } found;'
+    }, display_index_noindex);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: "var cloud = document.querySelector(\"script[src*='netpeak.cloud/js/init']\").src; var cloud_id = cloud.match(/\\d+/)[0]; cloud_id;"
+    }, display_netpeak_cloud);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: "if(document.querySelectorAll('h1').length>1) { 'Внимание! Повече от един H1 елемента на страницата!' } "
+    }, display_h1_error);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: "window.location.origin"
+    }, robots_links);
+
+    chrome.tabs.executeScript(tab.id, {
+        code: "var req = new XMLHttpRequest(); req.open('GET', document.location, false); req.send(null); var headers = req.getAllResponseHeaders(); headers.split(\"\\n\")"
+    }, http_headers);
+
+    display_long_meta_error(tab_title);
+
+});
+
