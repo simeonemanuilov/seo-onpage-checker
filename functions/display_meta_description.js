f2f9c1aef9bc4665b2ce31141517b1740c290a40
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2019-01-14 23:02:00 +0200

    Improvement in description function

--- functions/display_meta_description.js
+++ functions/display_meta_description.js
@@ -1,7 +1,7 @@
 var metas = document.getElementsByTagName("meta");
 var meta_description = "";
 for (var i = 0; i < metas.length; i++) {
-    if (metas[i].getAttribute("name") == "description") {
+    if (metas[i].getAttribute("name") == "description" || metas[i].getAttribute("name") == "DESCRIPTION") {
         meta_description = metas[i].getAttribute("content");
     }
 }
