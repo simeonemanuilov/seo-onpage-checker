083dc56dd5d65654c1a6cf5b372671a004fd101d
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2019-01-14 22:59:13 +0200

    Improvement in canonical function

--- functions/display_canonical.js
+++ functions/display_canonical.js
@@ -2,7 +2,7 @@ var url = document.location;
 var links = document.getElementsByTagName("link");
 var found = "none";
 for (var i = 0, l; l = links[i]; i++) {
-    if (l.getAttribute("rel") == "canonical") {
+    if (l.getAttribute("rel") == "canonical" || l.getAttribute("rel") == "CANONICAL") {
         found = l.getAttribute("href");
         break;
     }
