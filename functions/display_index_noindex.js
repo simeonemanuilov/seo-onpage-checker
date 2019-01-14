2ae18b64a6888cda33e48706081a7eb8da0746ac
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2019-01-14 22:57:13 +0200

    Changes in index/noindex function

--- functions/display_index_noindex.js
+++ functions/display_index_noindex.js
@@ -1,8 +1,8 @@
 var url = document.location;
 var links = document.getElementsByTagName("meta");
-var found = "none";
+var found = "index";
 for (var i = 0, l; l = links[i]; i++) {
-    if (l.getAttribute("name") == "robots") {
+    if (l.getAttribute("name") == "robots" || l.getAttribute("name") == "ROBOTS") {
         found = l.getAttribute("content");
         break;
     }
